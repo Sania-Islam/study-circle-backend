3abca64bf327f4a596aaa8f0f9c07ca180e5025b
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-07-31 11:55:43 +0600

    Update noteRoutes.js

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -3,14 +3,13 @@ const {
   uploadNote,
   getNotesForCourse,
   downloadNote,
+  deleteNote,
 } = require("../controllers/noteController");
 const { protect } = require("../middleware/auth");
 const { upload } = require("../middleware/upload");
-
 const router = express.Router();
-
 router.get("/courses/:id/notes", getNotesForCourse);
 router.post("/courses/:id/notes", protect, upload.single("file"), uploadNote);
 router.get("/notes/:id/download", downloadNote);
-
+router.delete("/notes/:id", protect, deleteNote);
 module.exports = router;
